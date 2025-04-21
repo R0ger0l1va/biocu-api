d156081215c2aacc456bc06ab4fc6327ba50dbe9
Roger Oliva <rogeroliva44@gmail.com>
2025-04-20 20:05:54 -0400

    a
    a

--- prisma/migrations/20250421000022_de_nuevo/migration.sql
+++ prisma/migrations/20250421000022_de_nuevo/migration.sql
@@ -1,29 +1,31 @@
 -- CreateTable
 CREATE TABLE "reportes" (
-    "id" UUID NOT NULL DEFAULT uuid_generate_v4(),
+    "id" UUID NOT NULL,
     "titulo" VARCHAR(100) NOT NULL,
     "direccion" VARCHAR(255) NOT NULL,
     "descripcion" TEXT NOT NULL,
     "latitud" DECIMAL(10,8) NOT NULL,
     "longitud" DECIMAL(11,8) NOT NULL,
-    "imagen_url" VARCHAR(255)[],
     "estado" VARCHAR(20) NOT NULL DEFAULT 'sin_revisar',
     "usuario_id" UUID NOT NULL,
     "fecha_creacion" TIMESTAMP(6) DEFAULT CURRENT_TIMESTAMP,
     "fecha_actualizacion" TIMESTAMP(6) DEFAULT CURRENT_TIMESTAMP,
+    "imagenes" VARCHAR(255)[],
 
     CONSTRAINT "reportes_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "usuarios" (
-    "id" UUID NOT NULL DEFAULT uuid_generate_v4(),
+    "id" UUID NOT NULL,
     "nombre" VARCHAR(100) NOT NULL,
     "email" VARCHAR(100) NOT NULL,
     "password_hash" VARCHAR(255) NOT NULL,
     "role" VARCHAR(255) NOT NULL DEFAULT 'user',
     "fecha_creacion" TIMESTAMP(6) DEFAULT CURRENT_TIMESTAMP,
     "fecha_actualizacion" TIMESTAMP(6) DEFAULT CURRENT_TIMESTAMP,
+    "resetToken" TEXT,
+    "resetTokenExpiry" TIMESTAMP(3),
 
     CONSTRAINT "usuarios_pkey" PRIMARY KEY ("id")
 );
